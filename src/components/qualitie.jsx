119b9f4248b2bc9f5aa007658a1f8893a3a64812
IrinaBl <super_ya93@bk.ru>
2023-02-02 17:47:03 +0400

    filtration

--- src/components/qualitie.jsx
+++ src/components/qualitie.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Qualitie = ({ color, name, _id }) => {
-  return (
-    <span className={`badge bg-${color} m-2`} key={_id}>
-      {name}
-    </span>
-  );
+const Qualitie = ({ color, name }) => {
+  return <span className={`badge bg-${color} m-2`}>{name}</span>;
 };
 
 Qualitie.propTypes = {
   color: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  _id: PropTypes.number.isRequired
+  name: PropTypes.string.isRequired
+  // _id: PropTypes.number.isRequired
 };
 
 export default Qualitie;
