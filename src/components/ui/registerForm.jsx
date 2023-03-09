b3a7df3104c3039743626342c0dc50c82c534fe1
IrinaBl <super_ya93@bk.ru>
2023-03-09 23:44:37 +0400

    checkbox

--- src/components/ui/registerForm.jsx
+++ src/components/ui/registerForm.jsx
@@ -4,20 +4,29 @@ import TextField from "../common/form/textField";
 import api from "../../api";
 import SelectField from "../common/form/selectField";
 import RadioField from "../common/form/radioField";
+import MultiSelectField from "../common/form/multiSelectField";
+import CheckBoxField from "../common/form/checkBoxField";
 
 const RegisterForm = () => {
   const [data, setData] = useState({
     email: "",
     password: "",
     profession: "",
-    sex: "male"
+    sex: "male",
+    qualities: [],
+    licence: false
   });
+
+  const [qualities, setQualities] = useState({});
   const [professions, setProfession] = useState();
   const [errors, setErrors] = useState({});
+
   useEffect(() => {
     api.professions.fetchAll().then((data) => setProfession(data));
+    api.qualities.fetchAll().then((data) => setQualities(data));
   }, []);
-  const handleChange = ({ target }) => {
+
+  const handleChange = (target) => {
     setData((prevState) => ({ ...prevState, [target.name]: target.value }));
   };
 
@@ -44,6 +53,12 @@ const RegisterForm = () => {
     profession: {
       isRequired: {
         message: "Обязательно выберите вашу профессию"
+      }
+    },
+    licence: {
+      isRequired: {
+        message:
+          "Вы не можете использовать наш сервиз без подтверждения лицензионного соглащения"
       }
     }
   };
@@ -102,8 +117,22 @@ const RegisterForm = () => {
         value={data.sex}
         name="sex"
         onChange={handleChange}
+        label="Выберите ваш пол"
       />
-
+      <MultiSelectField
+        options={qualities}
+        onChange={handleChange}
+        name="qualities"
+        label="Выберите ваши качества"
+      />
+      <CheckBoxField
+        value={data.licence}
+        onChange={handleChange}
+        name="licence"
+        error={errors.licence}
+      >
+        Подтвердить <a>лицензионное соглашение</a>
+      </CheckBoxField>
       <button
         type="submit"
         disabled={!isValid}
